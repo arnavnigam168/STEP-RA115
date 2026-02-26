a809a40fedbbe2e86d277a4d9d0dd3e043712c1c
arnavnigam168 <code.arnavnigam@gmail.com>
2026-02-26 10:09:54 +0530

    UC11 - Object-Oriented Palindrome Service

---
+++ src/UseCase11PalindromeCheckerApp.java
@@ -0,0 +1,21 @@
+public class UseCase11PalindromeCheckerApp {
+    public static void main(String[] args) {
+        PalindromeService service = new PalindromeService();
+        String word = "deified";
+        if (service.checkPalindrome(word)) {
+            System.out.println("The given string \"" + word + "\" is a Palindrome.");
+        } else {
+            System.out.println("The given string \"" + word + "\" is not a Palindrome.");
+        }
+    }
+}
+
+class PalindromeService {
+    public boolean checkPalindrome(String word) {
+        String reversed = "";
+        for (int i = word.length() - 1; i >= 0; i--) {
+            reversed = reversed + word.charAt(i);
+        }
+        return word.equals(reversed);
+    }
+}
