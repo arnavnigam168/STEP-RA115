05f2b92f5a896b19e6c6a0cfb7d0b50b0aef08a1
arnavnigam168 <code.arnavnigam@gmail.com>
2026-02-26 10:41:17 +0530

    File Name Changed

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -2,7 +2,7 @@ import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
-public class UseCase6PalindromeCheckerApp {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         String word = "radar";
         Queue<Character> queue = new LinkedList<>();
