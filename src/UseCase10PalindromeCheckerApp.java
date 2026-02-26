017c44f9e81aa5690383f889f3fced1c9e8d5e53
arnavnigam168 <code.arnavnigam@gmail.com>
2026-02-26 10:08:47 +0530

    UC10 -  Case-Insensitive & Space-Ignored Palindrome

---
+++ src/UseCase10PalindromeCheckerApp.java
@@ -0,0 +1,17 @@
+public class UseCase10PalindromeCheckerApp {
+    public static void main(String[] args) {
+        String original = "A man a plan a canal Panama";
+        String normalized = original.replaceAll("\\s+", "").toLowerCase();
+        String reversed = "";
+
+        for (int i = normalized.length() - 1; i >= 0; i--) {
+            reversed = reversed + normalized.charAt(i);
+        }
+
+        if (normalized.equals(reversed)) {
+            System.out.println("The given string \"" + original + "\" is a Palindrome.");
+        } else {
+            System.out.println("The given string \"" + original + "\" is not a Palindrome.");
+        }
+    }
+}
